95a5133c761fe222b46db74d4d3d5a6c3ca9f504
Samuele Ribaudo <samuele.ribaudo@tum.de>
2026-02-28 20:25:53 +0100

    Implemented decision making package with state machine

---
+++ ros2_ws/src/decision_making_pkg/src/state_machine.cpp
@@ -0,0 +1,103 @@
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <cmath>
+
+using namespace std::chrono_literals;
+
+enum class MissionState {
+    WAYPOINT_HOVER,
+    APPROACH_CAVE,
+    MISSION_COMPLETED,
+};
+
+class StateMachineNode : public rclcpp::Node {
+public:
+    StateMachineNode() : Node("state_machine_node"), current_state_(MissionState::WAYPOINT_HOVER), has_odom_(false), target_sent_(false) {
+        target_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("/next_setpoint", 10);
+        
+        odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+            "/current_state_est", 10, [this](const nav_msgs::msg::Odometry::SharedPtr msg) {
+                current_pos_ = msg->pose.pose.position;
+                current_ori_ = msg->pose.pose.orientation;
+                has_odom_ = true;
+            });
+
+        mission_timer_ = this->create_wall_timer(500ms, std::bind(&StateMachineNode::stateMachineTick, this));
+        RCLCPP_INFO(this->get_logger(), "State Machine Ready");
+    }
+
+private:
+    void stateMachineTick() {
+        if (!has_odom_) return;
+
+        switch (current_state_) {
+            case MissionState::WAYPOINT_HOVER:
+                if (!target_sent_) {
+                    sendTarget(-36.0, 10.0, 25.0, 0.0, 0.0, 1.0, 0.0);
+                    RCLCPP_INFO(this->get_logger(), "Climbing...");
+                    target_sent_ = true;
+                }
+                
+                if (isReached(-36.0, 10.0, 25.0)) {
+                    current_state_ = MissionState::APPROACH_CAVE;
+                    target_sent_ = false;
+                }
+                break;
+
+            case MissionState::APPROACH_CAVE:
+                if (!target_sent_) {
+                    sendTarget(-315.14, 8.38, 15.0);
+                    RCLCPP_INFO(this->get_logger(), "Approaching cave entrance...");
+                    target_sent_ = true;
+                }
+                
+                if (isReached(-315.14, 8.38, 15.0)) {
+                    current_state_ = MissionState::MISSION_COMPLETED; 
+                }
+                break;
+
+            case MissionState::MISSION_COMPLETED:
+                RCLCPP_INFO_ONCE(this->get_logger(), "Mission completed");
+                break;
+        }
+    }
+
+
+    void sendTarget(double x, double y, double z, double qx = 0.0, double qy = 0.0, double qz = 0.0, double qw = 0.0) {
+        auto msg = geometry_msgs::msg::PoseStamped();
+        msg.header.stamp = this->now();
+        msg.header.frame_id = "world";
+        msg.pose.position.x = x;
+        msg.pose.position.y = y;
+        msg.pose.position.z = z;
+        msg.pose.orientation.x = qx;
+        msg.pose.orientation.y = qy;
+        msg.pose.orientation.z = qz;
+        msg.pose.orientation.w = qw;
+        target_pub_->publish(msg);
+    }
+
+    bool isReached(double tx, double ty, double tz) {
+        double dist = std::sqrt(std::pow(tx - current_pos_.x, 2) +
+                                std::pow(ty - current_pos_.y, 2) +
+                                std::pow(tz - current_pos_.z, 2));
+        return dist < 1.5;
+    }
+
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr target_pub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::TimerBase::SharedPtr mission_timer_;
+    geometry_msgs::msg::Point current_pos_;
+    geometry_msgs::msg::Quaternion current_ori_;
+    bool has_odom_, target_sent_;
+    MissionState current_state_;
+};
+
+int main(int argc, char** argv) {
+    rclcpp::init(argc, argv);
+    // Updated to match the renamed class StateMachineNode
+    rclcpp::spin(std::make_shared<StateMachineNode>());
+    rclcpp::shutdown();
+    return 0;
+}
