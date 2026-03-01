f0231beb5864b8791523a4dc15c80fc07bceb279
sheff <shukurilloyevfirdavs@gmail.com>
2026-03-01 21:32:47 +0100

    perception with only lantern detector

---
+++ ros2_ws/src/perception_pkg/setup.py
@@ -0,0 +1,33 @@
+import os               # <--- 1. ADD THIS
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'perception_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'lantern_detector = perception_pkg.lantern_detector:main',
+        ],
+    },
+)
